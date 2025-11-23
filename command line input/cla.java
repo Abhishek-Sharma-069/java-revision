1d44f2ebc74960cc088c512843a1f7d0d78ccf79
Abhishek Sharma <abhello12@gmail.com>
2025-11-23 19:43:13 +0530

    Command line argument

--- command line input/cla.java
+++ command line input/cla.java
@@ -5,7 +5,9 @@ public class cla {
     {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
 
-        System.out.println("Sum of CLA input =" + (x + y));
+        System.out.println("Sum of CLA 2 input :" + (x + y));
+        System.out.println("Sum of CLA 3 input :" + (x + y + z));
     }
 }
